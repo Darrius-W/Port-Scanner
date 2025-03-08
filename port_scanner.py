3cb82fdb7c9e9cf03cf1b619182bbca7954aec9e
Darrius-W <darriuswhite435@gmail.com>
2025-03-07 22:52:58 -0600

    Added use of command line args and port scan range

--- port_scanner.py
+++ port_scanner.py
@@ -1,24 +1,30 @@
 import socket
 import sys
 
-# Accept input with command line args
-host_ip = str(sys.argv[1])
-port = int(sys.argv[2])
-
+if len(sys.argv) == 4:
+    # Accept input with command line args
+    host_ip = str(sys.argv[1])
+    port_start = int(sys.argv[2])
+    port_end = int(sys.argv[3])
+else:
+    print("Invalid amount of Arguments")
+    sys.exit()
+    
 # Function to scan individual host ports 
-def port_scanner(host_ip, port):
+def port_scanner(host_ip, port_start, port_end):
     try:
-        # Create socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1) # set timeout for connection to ip so wont loop infinitely
-
-        # Attempt a network connection
-        result = sock.connect_ex((host_ip, port))
+        for port in range(port_start, port_end+1):
+            # Create socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1) # set timeout for connection to ip so wont loop infinitely
         
-        if result == 0:
-            print("Port {} is open".format(port))
-        else:
-            print("Connection to port failed")
+            # Attempt a network connection
+            result = sock.connect_ex((host_ip, port))
+            
+            if result == 0:
+                print("Port {} is open".format(port))
+            else:
+                print("Connection to port {} failed".format(port))
     
     except:
         print("Unable to connect to host {} on port {}".format(host_ip, port))
@@ -29,4 +35,4 @@ def port_scanner(host_ip, port):
         sock.close()
         
         
-port_scanner(host_ip, port)
+port_scanner(host_ip, port_start, port_end)
